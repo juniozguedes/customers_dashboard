b5a759b6d01a34479608f3e522578f2ba188f1dd
Wellington Guedes <wellington.guedes@afil.io>
2019-06-04 15:07:52 -0300

    Adicionando IP local

--- app.py
+++ app.py
@@ -14,4 +14,5 @@ db = SQLAlchemy(app)
 from views import *
 
 if __name__ == '__main__':  
-    app.run(debug=True, host='3.80.139.233', port=int("80"))
+    #app.run(debug=True, host='3.80.139.233', port=int("80"))
+    app.run(debug=True, host='localhost', port="5000")
